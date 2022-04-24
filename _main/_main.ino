aa4d10311f042653b3e03368e6736668cd4c6e6f
José Pedro <zemiguel.apedro@gmail.com>
2022-04-25 00:15:26 +0100

    Big push to complete interface.

--- _main/_main.ino
+++ _main/_main.ino
@@ -42,24 +42,56 @@ int myID = 0;
 
 IntFloat intfloat0, intfloat1;
 
-
 CommandParser parser(
     (Command[]){
-    {'a', "<float>", "reply is number + 2", floatArgCom([](float f){printI2C("a %d %f", myID, f+2)}), NULL},
-    {'h', "", "help", noArgCom(help), NULL},
+    {'a', "<int>", "sets the anti-windup state", intArgCom([](int val){if(val < 0 || val > 1) ERR controller.setOccupancy(val); ACK}), NULL},
+    {'b', "<int>", "sets the feedback state", intArgCom([](int val){if(val < 0 || val > 1) ERR controller.setFeedback(val); ACK}), NULL},
+    {'B', "", "get last minute buffer", NULL, (Command[]){
+        {'l', "", "measured lumminance", notImplemented, NULL},
+        {'d', "", "duty cycle", notImplemented, NULL},
+        {'\0', "", "", NULL, NULL}}},
     {'C', "", "calibration utilities", NULL, (Command[]){
         {'a', "", "calibrate gamma and tau, save these parameters to EEPROM and activate them", noArgCom(calibrateAutoCommand), NULL},
         {'c', "<bool: gamma> <bool: tau>", "calibrates gamma and/or tau and saves the calibrated parameters", calibrateCommand, NULL},
         {'s', "<bool: gamma> <bool: tau>", "saves gamma and/or tau from calibrated parameters as final (writes to EEPROM)", saveCalibrationCommand, NULL},
         {'p', "", "print latest calibrated parameters and active parameters", noArgCom(printCalibratedCommand), NULL},
         {'\0', "", "", NULL, NULL}}},
+    {'d', "<float>", "sets the duty cycle", floatArgCom([](float duty){if(duty < 0 || duty > 1) ERR controller.turnControllerOff(); set_u(duty); ACK}), NULL},
     {'g', "", "get command", NULL, (Command[]){
-        {'r', "", "reference", printfCom("reference :)"), NULL},
+        {'a', "", "anti-windup", printfCom("a %d %d", myID, controller.getAntiWindup()), NULL},
+        {'b', "", "feedback", printfCom("b %d %d", myID, controller.getFeedback()), NULL},
+        {'c', "", "current energy cost", notImplemented, NULL},
+        {'d', "", "duty cycle", printfCom("d %d %f", myID, controller.getSample().u), NULL},
+        {'e', "", "accumulated energy", printfCom("e %d %f", myID, energy), NULL},
+        {'f', "", "accumulated flicker error", printfCom("f %d %f", myID, flickerAccumulator), NULL},
+        {'F', "", "average flicker error", printfCom("f %d %f", myID, flickerAccumulator / sampleNumber), NULL},
+        {'l', "", "measured luminance", printfCom("l %d %f", myID, controller.getSample().L), NULL},
+        {'L', "", "luminance lower bound", notImplemented, NULL},
+        {'o', "", "occupancy", printfCom("o %d %d", myID, controller.getOccupancy()), NULL},
+        {'O', "", "occupied luminance lower bound", notImplemented, NULL},
+        {'p', "", "instantaneous power", printfCom("p %d %f", myID, controller.getSample().u), NULL},
+        {'r', "", "reference", printfCom("r %d %f", myID, controller.getSample().Reference), NULL},
+        {'t', "", "time since restart", printfCom("t %d %lu", myID, to_ms_since_boot(get_absolute_time()) / 1000), NULL},
+        {'U', "", "unoccupied luminance lower bound", notImplemented, NULL},
+        {'v', "", "accumulated visibility error", printfCom("v %d %f", myID, visibilityAccumulator), NULL},
+        {'V', "", "average visiblity error", printfCom("f %d %f", myID, visibilityAccumulator / sampleNumber), NULL},
+        {'w', "", "feedforward", printfCom("w %d %d", myID, controller.getFeedforward()), NULL},
+        {'x', "", "external luminance", printfCom("x %d %f", myID, controller.getSample().L - gain * controller.getSample().u), NULL},
         {'\0', "", "", NULL, NULL}}},
-    {'p', "", "ack", noArgCom([](){ACK}), NULL},
-    {'t', "", "time", printfCom("t %d %lu", myID, millis()), NULL},
+    {'h', "", "help", noArgCom(help), NULL},
     {'k', "<int>", "callibrator gain <id> to <id>", intArgCom([](int id) {printI2C("k %d %f", myID, calibrator.getGainId(id)) }), NULL},
-    {'r', "", "system reset", noArgCom([](){__NVIC_SystemReset(); ACK}), NULL},
+    {'o', "<int>", "sets the occupancy state", intArgCom([](int val){if(val < 0 || val > 1) ERR controller.setOccupancy(val); ACK}), NULL},
+    {'O', "<float>", "sets the occupied lower bound lumminance", notImplemented, NULL},
+    {'p', "", "ack", noArgCom([](){ACK}), NULL},
+    {'r', "<float>", "sets the reference", floatArgCom([](float reference){if(reference < 0) ERR controller.setReference(reference); ACK}), NULL},
+    {'R', "", "system reset", noArgCom([](){__NVIC_SystemReset(); ACK}), NULL},
+    {'s', "", "stream variable", NULL, (Command[]){
+        {'l', "", "measured lumminance", notImplemented, NULL},
+        {'d', "", "duty cycle", notImplemented, NULL},
+        {'\0', "", "", NULL, NULL}}},
+    {'t', "", "time", printfCom("t %d %lu", myID, millis()), NULL},
+    {'U', "<float>", "sets the unoccupied lower bound lumminance", notImplemented, NULL},
+    {'w', "<int>", "sets the feedforward state", intArgCom([](int val){if(val < 0 || val > 1) ERR controller.setFeedforward(val); ACK}), NULL},
     {'\0', "", "", NULL, NULL}
 }
 );
@@ -95,4 +127,13 @@ void loop() {
         parseSerial(comms);
 
     comms.eventLoop();
+}
+
+void setup1() {
+    core1AlarmPool = alarm_pool_create(1, 1);
+    controller.setup(0.01, 0.05);
+}
+
+void loop1() {
+
 }
