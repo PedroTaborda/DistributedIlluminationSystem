70ae95910198ec659ed6fc893ce65a8e14f0a68c
José Pedro <zemiguel.apedro@gmail.com>
2022-04-24 16:50:24 +0100

    Integrate Diogo's controller

--- _main/controller.cpp
+++ _main/controller.cpp
@@ -1,15 +1,11 @@
 #include "controller.hpp"
 
 #include "globals.hpp"
-#include "interface.hpp"
 #include "utilities.hpp"
 
-Controller::Controller()
-{
+Controller::Controller() {}
 
-}
-
-void Controller::setup(float proportionalGain, float integralGain, repeating_timer *timerStruct) volatile{
+void Controller::setup(float proportionalGain, float integralGain, repeating_timer *timerStruct) volatile {
     this->proportionalGain = proportionalGain;
     this->integralGain = integralGain;
 
@@ -20,44 +16,53 @@ void Controller::setup(float proportionalGain, float integralGain, repeating_tim
     feedforward = true;
     controllerOn = true;
     simulatorOn = true;
-    reference[0] = 0; reference[1] = 0;
-    dutyCycle[0] = 0; dutyCycle[1] = 0;
+    reference[0] = 0;
+    reference[1] = 0;
+    dutyCycle = 0;
     occupancy = 0;
 
-    simulator.initialize(micros(), measureVoltage(10), dutyCycle[occupancy]);
+    latest_sample.reset();
 
-    int64_t delayUs = (int64_t) (samplingTime * 1e6);
+    simulator.initialize(micros(), measureVoltage(10), dutyCycle);
+
+    int64_t delayUs = (int64_t)(samplingTime * 1e6);
 
     alarm_pool_add_repeating_timer_us(core1AlarmPool, -delayUs, Controller::controllerLoop, (void *)this, timerStruct);
 }
 
 bool Controller::controllerLoop(repeating_timer *timerStruct) {
-    volatile Controller* controller = (volatile Controller*)timerStruct->user_data;
+    /* must be static because repeating timer callbacks can only have one argument */
+    volatile Controller *controller = (volatile Controller *)timerStruct->user_data;
     float reference = controller->reference[controller->occupancy];
+    unsigned long t;
+    controller->handle_requests();
+
+    t = micros();
+    controller->sampleDuration = (t - controller->lastTimestamp);
+    controller->lastTimestamp = t;
 
     // Measure the current luminance value and get the current simulator prediction
     float voltage = measureVoltage(5);
-    float currentLux = LDRVoltageToLux(voltage);
+    controller->currentLux = LDRVoltageToLux(voltage);
     float feedbackReference = 0.f;
-    controller->simulatorValue = controller->simulator.getLuminosity(micros());
-    if(controller->simulatorOn)
+    controller->simulatorValue = controller->simulator.getLuminosity(t);
+    if (controller->simulatorOn)
         feedbackReference = controller->simulatorValue;
     else
         feedbackReference = reference;
 
-    if(controller->controllerOn) {
+    if (controller->controllerOn) {
         float feedforwardTerm = 0.f;
         float feedbackTerm = 0.f;
         float proportionalTerm = 0.f;
         float integralTerm = 0.f;
         float dutyCycle = 0.f;
 
-        if(controller->feedforward)
-            feedforwardTerm = (reference - ambientIlluminance) / gain;
+        if (controller->feedforward) feedforwardTerm = (reference - ambientIlluminance) / gain;
 
-        if(controller->feedback) {
+        if (controller->feedback) {
             // Compute error
-            controller->trackingError = feedbackReference - currentLux;
+            controller->trackingError = feedbackReference - controller->currentLux;
 
             // Compute proportional and integral terms
             proportionalTerm = controller->proportionalGain * controller->trackingError;
@@ -66,20 +71,20 @@ bool Controller::controllerLoop(repeating_timer *timerStruct) {
             integralTerm = controller->integralGain * controller->integralError;
 
             feedbackTerm = proportionalTerm + integralTerm;
-            
+
             dutyCycle = feedbackTerm + feedforwardTerm;
 
             // Implement Anti-Windup by saturating the integral term
-            if(controller->antiWindup && !fequal(controller->integralGain, 0.f)) {
+            if (controller->antiWindup && !fequal(controller->integralGain, 0.f)) {
                 float dutyMin = 0.0f;
                 float dutyMax = 1.0f;
 
-                if(dutyCycle < dutyMin) {
+                if (dutyCycle < dutyMin) {
                     integralTerm = dutyMin - feedforwardTerm - proportionalTerm;
                     controller->integralError = integralTerm / controller->integralGain;
-                } 
-                
-                if(dutyCycle > dutyMax) {
+                }
+
+                if (dutyCycle > dutyMax) {
                     integralTerm = dutyMax - feedforwardTerm - proportionalTerm;
                     controller->integralError = integralTerm / controller->integralGain;
                 }
@@ -89,115 +94,132 @@ bool Controller::controllerLoop(repeating_timer *timerStruct) {
 
         dutyCycle = feedforwardTerm + feedbackTerm;
 
-        if(dutyCycle < 0.f) dutyCycle = 0.f;
-        if(dutyCycle > 1.f) dutyCycle = 1.f;
-        controller->dutyCycle[controller->occupancy] = dutyCycle;
+        if (dutyCycle < 0.f) dutyCycle = 0.f;
+        if (dutyCycle > 1.f) dutyCycle = 1.f;
+        controller->dutyCycle = dutyCycle;
+
+        set_u(controller->dutyCycle);
     }
 
-    analogWrite(LED_PIN, (int)(controller->dutyCycle[controller->occupancy] * DAC_RANGE));
-    controller->jitter = (micros() - controller->lastTimestamp) - ((int)(controller->samplingTime * 1e6));
-    controller->lastTimestamp = micros();
+    energy += controller->dutyCycle * controller->samplingTime;
+    visibilityAccumulator += max(0, reference - controller->currentLux);
+    double currentFlicker = controller->currentLux - previousLux;
 
-    energy += controller->dutyCycle[controller->occupancy] * controller->samplingTime;
-    visibilityAccumulator += max(0, reference - currentLux);
-    double currentFlicker = currentLux - previousLux;
-
-    if(currentFlicker * previousFlicker < 0)
+    if (currentFlicker * previousFlicker < 0)
         flickerAccumulator += (abs(currentFlicker) + abs(previousFlicker)) / (2 * controller->samplingTime);
 
-    previousLux = currentLux;
+    previousLux = controller->currentLux;
     previousFlicker = currentFlicker;
 
     sampleNumber += 1;
 
-    dutyBuffer.insert(controller->dutyCycle[controller->occupancy]);
-    luminanceBuffer.insert(currentLux);
+    dutyBuffer.insert(controller->dutyCycle);
+    luminanceBuffer.insert(controller->currentLux);
 
-    streamer.setNewValues();
-
+    controller->update_outputs();
     return true;
 }
 
-void Controller::changeSimulatorReference(float reference) volatile{
+void Controller::handle_requests() volatile {
+    /* Handle requests from messenger core */
+    controllerOn = control_on_req;
+    occupancy = occupancy_req;
+    if (new_ref) {
+        reference[occupancy] = refL_mlux / 1000.0;
+        changeSimulatorReference(reference[occupancy]);
+
+        // only let messenger core continue after all is written
+        new_ref = false;
+    }
+}
+
+void Controller::update_outputs() volatile {
+    volatile sample_t newsample;
+    newsample.dur = sampleDuration;
+    newsample.L = currentLux;
+    newsample.IntegralError = integralError;
+    newsample.TrackingError = trackingError;
+    newsample.SimulatorValue = simulatorValue;
+    newsample.Reference = reference[occupancy];
+    newsample.num = sampleNumber;
+
+    latest_sample.insert(newsample);
+}
+
+void Controller::changeSimulatorReference(float reference) volatile {
     float dutyCycle = (reference - ambientIlluminance) / gain;
 
-    if(dutyCycle > 1.0f) dutyCycle = 1.0f;
-    if(dutyCycle < 0.0f) dutyCycle = 0.0f;
+    if (dutyCycle > 1.0f) dutyCycle = 1.0f;
+    if (dutyCycle < 0.0f) dutyCycle = 0.0f;
 
     simulator.changeInput(micros(), dutyCycle, measureVoltage(10));
 }
 
-float Controller::getReference() volatile {
-    return reference[occupancy];
+// Interface functions to be accessed by messenger core
+
+int Controller::getSampleNumber() volatile {
+    if (!latest_sample.available())
+        return -1;
+    else
+        return latest_sample.getBegin(1).num;
 }
 
-int Controller::getOccupancy() volatile {
-    return occupancy;
-}
+volatile sample_t& Controller::getSample() volatile { return latest_sample.getBegin(1); }
+
+float Controller::getReference() volatile { return reference[occupancy]; }
+
+int Controller::getOccupancy() volatile { return occupancy; }
 
 void Controller::setReference(float reference) volatile {
-    controllerOn = true;
-    this->reference[occupancy] = reference;
-    changeSimulatorReference(reference);
+    /* Set illuminance reference (lux) and turn control on */
+    control_on_req = true;
+    refL_mlux = (int)(reference * 1000 + 0.5);  // +0.5 to round
+    new_ref = true;
+    while (new_ref)
+        ;  // wait for controller core to acknowledge new refL
 }
 
-void Controller::setDutyCycle(float duty) volatile {
-    controllerOn = false;
-    this->dutyCycle[occupancy] = duty;
-}
+/*void Controller::setDutyCycle(float duty) volatile {
+    turnControllerOff();
+    dutyCycle[occupancy] = duty;
+    set_u(duty);
+}*/
 
-void Controller::setOccupancy(int occupancy) volatile {
-    this->occupancy = occupancy;
+void Controller::setOccupancy(int occupancy_in) volatile {
+    occupancy_req = occupancy_in;
+    while (occupancy != occupancy_in)
+        ;  // wait for occupancy to change
 }
 
 void Controller::turnControllerOff() volatile {
-    controllerOn = false;
+    control_on_req = false;
+    while (controllerOn)
+        ;  // wait for controller core to turn off controller
 }
 
-void Controller::turnControllerOn() volatile {
-    controllerOn = true;
-}
+void Controller::turnControllerOn() volatile { control_on_req = true; }
 
-void Controller::toggleAntiWindup() volatile {
-    antiWindup = !antiWindup;
-}
+void Controller::toggleAntiWindup() volatile { antiWindup = !antiWindup; }
 
-void Controller::toggleFeedback() volatile {
-    feedback = !feedback;
-}
+void Controller::toggleFeedback() volatile { feedback = !feedback; }
 
-void Controller::toggleFeedforward() volatile {
-    feedforward = !feedforward;
-}
+void Controller::toggleFeedforward() volatile { feedforward = !feedforward; }
 
-void Controller::setSimulator(int simulator) volatile {
-    simulatorOn = simulator == 0 ? false : true;
-}
+void Controller::setSimulator(int simulator) volatile { simulatorOn = simulator == 0 ? false : true; }
 
-void Controller::setProportionalGain(float proportionalGain) volatile{
-    this->proportionalGain = proportionalGain;
-}
+void Controller::setProportionalGain(float proportionalGain_in) volatile { proportionalGain = proportionalGain_in; }
 
-void Controller::setIntegralGain(float integralGain) volatile{
-    this->integralGain = integralGain;
-}
+void Controller::setIntegralGain(float integralGain_in) volatile { integralGain = integralGain_in; }
 
-bool Controller::getAntiWindup() volatile{
-    return antiWindup;
-}
+bool Controller::getAntiWindup() volatile { return antiWindup; }
 
-bool Controller::getFeedback() volatile{
-    return feedback;
-}
+bool Controller::getFeedback() volatile { return feedback; }
 
-bool Controller::getFeedforward() volatile{
-    return feedforward;
-}
+bool Controller::getFeedforward() volatile { return feedforward; }
 
-float Controller::getProportionalGain() volatile{
-    return proportionalGain;
-}
+float Controller::getProportionalGain() volatile { return proportionalGain; }
 
-float Controller::getIntegralGain() volatile{
-    return integralGain;
-}
+float Controller::getIntegralGain() volatile { return integralGain; }
+
+volatile Controller controller;
+repeating_timer timerStruct;
--- _main/controller.hpp
+++ _main/controller.hpp
@@ -1,18 +1,38 @@
-#ifndef CONTROLLER_HPP
-#define CONTROLLER_HPP
-
-#include <utility>
+#ifndef CONTROLLER2_HPP
+#define CONTROLLER2_HPP
 
 #include <Arduino.h>
+
+#include <utility>
 
 #include "buffer.hpp"
 #include "simulator.hpp"
 
-class VariableStream;
+struct sample_t {
+    // Overload assignment operator for volatile type
+    void operator=(volatile sample_t& a) volatile {
+        dur = a.dur;
+        L = a.L;
+        u = a.u;
+        IntegralError = a.IntegralError;
+        TrackingError = a.TrackingError;
+        SimulatorValue = a.SimulatorValue;
+        Reference = a.Reference;
+        num = a.num;
+    }
+    unsigned long dur;  // in us
+    float L;            // in lux
+    float u;
+    float IntegralError;
+    float TrackingError;
+    float SimulatorValue;
+    float Reference;
+
+    int num;
+};
 
 class Controller {
 public:
-
     // Default Constructor
     Controller();
 
@@ -23,12 +43,13 @@ public:
     static bool controllerLoop(repeating_timer *timerStruct);
 
     // User interaction functions
-    void changeSimulatorReference(float reference) volatile;
     float getReference() volatile;
     int getOccupancy() volatile;
     void setReference(float reference) volatile;
-    void setDutyCycle(float duty) volatile;
+    //void setDutyCycle(float duty) volatile;
     void setOccupancy(int occupancy) volatile;
+    int getSampleNumber() volatile;  // returns number of latest sample, returns negative if none is saved
+    volatile sample_t& getSample() volatile;
 
     // Controller settings
     void turnControllerOff() volatile;
@@ -47,27 +68,50 @@ public:
     float getIntegralGain() volatile;
 
 private:
+    void handle_requests() volatile;
+    void update_outputs() volatile;
+    void changeSimulatorReference(float reference) volatile;
 
-    friend VariableStream;
+    // variables marked with I are not to be written to by controller core during execution
+    // they can be changed at any time by users of this class through interface functions
 
     // Controller state
     float integralError;
     Simulator simulator;
-    bool controllerOn;
-    bool simulatorOn;
+    bool controllerOn;  // I
+    bool simulatorOn;   // I
     int occupancy;
     float reference[2];
-    float dutyCycle[2];
+    float dutyCycle;
+    float currentLux;
+
+    // sample buffer of size 2 such that controller can write to one position while the class user
+    // reads the other position
+    Buffer<volatile sample_t, 2> latest_sample;
 
     // Controller auxiliary variables
     float trackingError, simulatorValue;
     unsigned long lastTimestamp;
-    long jitter;
-
+    unsigned long sampleDuration;
     // Controller parameters
-    float proportionalGain, integralGain;
     float samplingTime;
-    bool antiWindup, feedback, feedforward;
+    float proportionalGain, integralGain;    // I
+    bool antiWindup, feedback, feedforward;  // I
+
+    bool ControlLoopOn;  // true when timer is executing control loop
+
+    // interface variables, all marked with I
+    int occupancy_req;
+    int refL_mlux;        // in millis of lux
+    bool control_on_req;  // false -> request control off; true ->request control on
+    // ---------------------------
+
+    // these are written both by controller core and vy interface functions, synchonously
+    bool new_ref;
+    // ---------------------------
 };
 
-#endif //CONTROLLER_HPP
+// Controller Instance
+extern volatile Controller controller;
+
+#endif  // CONTROLLER_HPP
