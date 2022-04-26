c9bfca5b9866e34deee755f12df9807e6f125808
Diferante <diogo.f.antunes@tecnico.ulisboa.pt>
2022-04-26 20:24:41 +0100

    tester with zé values

--- _main/consensus_tester.cpp
+++ _main/consensus_tester.cpp
@@ -12,37 +12,38 @@ int not_actually_main()
 
     int nNodes = 3;
     double localCost1 = 1;
-    double localCost2 = 3;
-    double localCost3 = 2;
-    double gainsOthersToMe1[3] = {200.0, 50.0, 20.0};
-    double gainsOthersToMe2[3] = {50.0, 200.0, 60.0};
+    double localCost2 = 1;
+    double localCost3 = 1;
+    double gainsOthersToMe1[3] = {200.0, 50.0, 50.0};
+    double gainsOthersToMe2[3] = {50.0, 200.0, 50.0};
     double gainsOthersToMe3[3] = {50.0, 30.0, 200.0};
-    solver1.li = 20;
-    solver1.oi = 0.906901;
+    solver1.li = 100;
+    solver1.oi = 20;
     solver1.rho = 7.0;
 
     solver2.li = 0;
-    solver2.oi = 0.881894;
+    solver2.oi = 30;
     solver2.rho = 7.0;
 
     solver3.li = 0;
-    solver3.oi = 0.618491;
+    solver3.oi = 40;
     solver3.rho = 7.0;
 
     solver1.start(nNodes, 0, localCost1, gainsOthersToMe1, 50);
     solver2.start(nNodes, 1, localCost2, gainsOthersToMe2, 50);
     solver3.start(nNodes, 2, localCost3, gainsOthersToMe3, 50);
 
-    for (int i = 0; i < 15; i++)
+    for (int i = 0; i < 20; i++)
     {
         printf("Iteration %d\n", i);
         double *sol1 = solver1.optimumSolution();
         double *sol2 = solver2.optimumSolution();
         double *sol3 = solver3.optimumSolution();
 
-        printf("Di1[%d]: %f, %f\n", i, sol1[0], sol1[1]);
-        printf("Di2[%d]: %f, %f\n", i, sol2[0], sol2[1]);
-        printf("Di2[%d]: %f, %f\n", i, sol3[0], sol3[1]);
+        printf("Di1[%d]: %f, %f, %f\n", i, sol1[0], sol1[1], sol1[2]);
+        printf("Di2[%d]: %f, %f, %f\n", i, sol2[0], sol2[1], sol2[2]);
+        printf("Di3[%d]: %f, %f, %f\n", i, sol3[0], sol3[1], sol3[2]);
+
 
         solver1.updateDiMean(sol1);
         solver1.updateDiMean(sol2);
